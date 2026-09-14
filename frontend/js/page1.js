aa8adf3b2040b31892b383fc1ef41302fba7e815
Tosiq <pnymeet@gmail.com>
2026-09-14 13:49:12 +0500

    js type

--- frontend/js/page1.js
+++ frontend/js/page1.js
@@ -1,3 +1,17 @@
+// --------- JASAVASCRIPT TYPES ---------
+// var n = "Ali Murtaza"                    => string
+// console.log(n)
+// var number = 34, 2342222222, 3.4522      => number
+// console.log(number)
+// var user = null                          => null
+// var x = undefined                        => undefined
+// var obj = { key: value, key: value }
+var product = { id: 22311, name: "Lays LG", price: 50, category: "Food" }
+// console.log(product, product.name)
+console.log(product.name, product.price)
+
+
+// -------------------------
 var a = 1
 var b = 4
 var sum = a+b
