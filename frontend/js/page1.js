85d24c5866a291c9c8a869d875cb3e9ebba4b5ea
Tosiq <pnymeet@gmail.com>
2026-09-15 12:48:57 +0500

    remaining types

--- frontend/js/page1.js
+++ frontend/js/page1.js
@@ -6,14 +6,30 @@
 // var user = null                          => null
 // var x = undefined                        => undefined
 // var obj = { key: value, key: value }
+var status = false // false
+// console.log(status)
 var product = { id: 22311, name: "Lays LG", price: 50, category: "Food" }
 // console.log(product, product.name)
-console.log(product.name, product.price)
+// console.log(product.name, product.price)
+// indexes    0           1         2         3    4     5
+var list = ["Lahore", "Karachi", "Multan", "alex", 23, {id: 1, status: true}]
+var data = [
+    {id: 1, name: "Food"},      // 0
+    {id: 2, name: "Wearing"},   // 1
+    {id: 3, name: "Sports"},    // 2
+    {id: 4, name: "Kids"},      // 3
+]
+// console.log(data[3])
 
+// regular express / /
+var EMAIL_PATTERN = /[a-z0-9\._%+!$&*=^|~#%'`?{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})/
+function printSum () {}
 
+var d = Date()
+console.log(d)
 // -------------------------
 var a = 1
 var b = 4
 var sum = a+b
-console.log("Ans is:", sum)
-console.log("I love pakistan")
+// console.log("Ans is:", sum)
+// console.log("I love pakistan")
