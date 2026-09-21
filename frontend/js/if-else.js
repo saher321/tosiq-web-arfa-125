9f8eef443a09c34c1019f3147b2b01e56ad73993
Tosiq <pnymeet@gmail.com>
2026-09-21 13:20:37 +0500

    nested if else

---
+++ frontend/js/if-else.js
@@ -0,0 +1,22 @@
+let useremail = "myemailemail.com"
+const EMAIL_REG = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+if (EMAIL_REG.test(useremail)){ // true
+    console.log("Email is correct")
+} else { // false
+    console.log("Given format is incorrect")
+}
+
+let userMark    = 45
+let totalMarks  = 70
+let bands       = 5
+
+if (userMark >= 40 && userMark <= totalMarks) {
+    if (bands >=6 && bands <= 8) {
+        console.log("You can apply now")
+    } else {
+        console.log("Band is low you can not apply")
+    }
+} else {
+    console.log("You can not apply for the admission")
+}
